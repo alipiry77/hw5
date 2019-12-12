6a11a9e3c21b99644f655f2e81a2b3b85c631aaa
alipiry77 <pirghazani.ali@gmail.com>
2019-12-12 13:08:30 +0330

    p2 added

---
+++ src/ir/maktab/java32/p2/Address.java
@@ -0,0 +1,30 @@
+package ir.maktab.java32.p2;
+
+public class Address {
+    String ally;
+    String street;
+    int zone;
+    String city;
+    long postalCode;
+
+    public Address(String ally, String street, int zone, String city, long postalCode) {
+        this.ally = ally;
+        this.street = street;
+        this.zone = zone;
+        this.city = city;
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "ally='" + ally + '\'' +
+                ", street='" + street + '\'' +
+                ", zone=" + zone +
+                ", city='" + city + '\'' +
+                ", postalCode=" + postalCode +
+                '}';
+    }
+
+
+}
---
+++ src/ir/maktab/java32/p2/Person.java
@@ -0,0 +1,43 @@
+package ir.maktab.java32.p2;
+
+import java.util.Comparator;
+
+public class Person implements Comparable<Person> {
+    int age;
+    String firstName;
+    String lastName;
+    Address address;
+
+    public Person(int age, String firstName, String lastName, Address address) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPerson{" +
+                "age=" + age +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                "\naddress=" + address +
+                '}';
+    }
+
+    public int compareTo(Person o) {
+        if(o.age>this.age){return -1;}
+        if(o.age<this.age){return +1;}
+        return 0;
+    }
+    public static class CompareByLastName implements Comparator<Person> {
+
+
+        public int compare(Person o1, Person o2) {
+            if(o1.lastName.length()<o2.lastName.length()){return -1;}
+            if(o1.lastName.length()>o2.lastName.length()){return +1;}
+            return 0;
+        }
+    }
+}
+
